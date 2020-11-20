91cb744b030d79ef4c81839f7ad533db86521b58
Isaac Johnson <45494522+geohci@users.noreply.github.com>
2020-11-19 19:31:32 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# list-building-interface
+Toolforge interface to allow users to explore various list-building approaches. 
+
+UI based on the following template: https://github.com/wikimedia/research-api-interface-template
+
+## License
+The source code for this interface is released under the [MIT license](https://github.com/geohci/wikidata-topic-model-api/blob/master/LICENSE).
+
+Screenshots of the results in the API may be used without attribution, but a link back to the application would be appreciated.
---
+++ app.py
@@ -0,0 +1,27 @@
+# Many thanks to: https://wikitech.wikimedia.org/wiki/Help:Toolforge/My_first_Flask_OAuth_tool
+
+import os
+import yaml
+
+from flask import Flask, render_template
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+__dir__ = os.path.dirname(__file__)
+app.config.update(
+    yaml.safe_load(open(os.path.join(__dir__, 'default_config.yaml'))))
+try:
+    app.config.update(
+        yaml.safe_load(open(os.path.join(__dir__, 'config.yaml'))))
+except IOError:
+    # It is ok if there is no local config file
+    pass
+
+# Enable CORS for API endpoints
+#CORS(app, resources={'*': {'origins': '*'}})
+CORS(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
